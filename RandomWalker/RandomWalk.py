018eb510065dec3ed21db51e5ac6b6e3640c466d
probabilityfilter <arunkumar@asu.edu>
2019-11-11 12:25:02 -0500

    Added results

--- RandomWalker/RandomWalk.py
+++ RandomWalker/RandomWalk.py
@@ -4,9 +4,9 @@ import random
 
 n = 100
 x = 0
-path = []
+path = [x]
 for i in range(n):
-    x += 2*random.randint(0, 1) - 1
+    x += 2 * random.randint(0, 1) - 1
     path.append(x)
 
 print(path)
