9c58e5fd084787ec75b2d334434c5ca712e08fd1
probabilityfilter <arunkumar@asu.edu>
2019-11-11 23:37:53 -0500

    Update variable names

--- RandomWalker/RandomWalk_1d_multiple walkers.py
+++ RandomWalker/RandomWalk_1d_multiple walkers.py
@@ -4,16 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def random_walk_1d(n):
+def random_walk_1d(steps, walkers):
     # Random choice of either +1 or -1 (go left or right)
-    steps = np.random.choice([-1, +1], (n, 50))  # Each of the 50 columns is a Walker
+    steps = np.random.choice(
+        [-1, +1], (steps, walkers)
+    )  # Each of the 50 columns is a Walker
     return np.cumsum(steps, axis=0)
 
 
-plt.plot(random_walk_1d(100000), lw=0.1)  # Plots 50 different lines (Walkers) on one plot
-plt.title('100,000 steps')  # subplot title
-plt.xticks([])
+plt.plot(
+    random_walk_1d(1000, 50), lw=0.1
+)  # Plots 50 different lines (Walkers) on one plot
+plt.title("Multiple Random Walkers in 1D, each with 100,000 steps")  # subplot title
+# plt.xticks([])
 
-plt.tight_layout()
-plt.suptitle('Multiple Random Walkers in 1D')  # main plot title
+""" plt.tight_layout()
+plt.suptitle("Multiple Random Walkers in 1D")  # main plot title """
 plt.show()
